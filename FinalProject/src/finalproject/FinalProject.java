251e234d6ae76b30ccc434e499bddf3a138cff57
Riley <gtxgam3r@gmail.com>
2015-03-28 17:03:22 -0400

    Main class file
    Changed the Heading on the main class file

--- FinalProject/src/finalproject/FinalProject.java
+++ FinalProject/src/finalproject/FinalProject.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Riley Boyd, Chris Hall
+ * Final Project for JavaFX
+ * *Idea Still Pending*
  */
 package finalproject;
 
