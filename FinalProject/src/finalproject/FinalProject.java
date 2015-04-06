3277616d3744eb1ac312e892556b5c8137ff52f7
ghost8202 <chrishall8202@hotmail.com>
2015-04-06 15:41:26 -0400

    update 1
    created a second class for editing adding
    also created methods for the action events on the buttons clicked

--- FinalProject/src/finalproject/FinalProject.java
+++ FinalProject/src/finalproject/FinalProject.java
@@ -6,13 +6,21 @@
  */
 package finalproject;
 
+
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -20,28 +28,70 @@ import javafx.stage.Stage;
  * @author Jewish Cunt
  */
 public class FinalProject extends Application {
-    
+    Second S= new Second();
     @Override
     public void start(Stage primaryStage) {
-        BorderPane pane = new BorderPane();
-        Button btn = new Button();
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(12);
+        /*BorderPane pane = new BorderPane();
+        VBox box=new VBox();
+        box.setPadding(new Insets(80,50,50,160));
+        box.setSpacing(30);
+        VBox box2=new VBox();
+        box.setPadding(new Insets(300,300,300,360));
+        box.setSpacing(30);*/
+        Button addBtn = new Button();
+        Button editBtn = new Button();
+        Button deleteBtn = new Button();
+        Button exitBtn = new Button();
         Button btn2 = new Button();
-        btn.setText("Say 'Hello World'");
-        pane.setLeft(btn);
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        addBtn.setText("Add");
+        editBtn.setText("Edit");
+        deleteBtn.setText("Delete");
+        exitBtn.setText("Exit");
+        HBox hbButtons=new HBox();
+        hbButtons.setSpacing(10);
+        /*pane.setLeft(addBtn);
+        pane.setRight(deleteBtn);
+        box.getChildren().add(addBtn);
+        box2.getChildren().add(deleteBtn);
+        Scene scene1=new Scene(box,300,250);
+        scene1.setFill(null);
+        pane.setLeft(exitBtn);*/
+       addBtn.setOnAction(new EventHandler<ActionEvent>() {
         
             @Override
             public void handle(ActionEvent event) {
                 
-                btn2.setText("Hey");
-                pane.setRight(btn2);
+                S.Add();
+                //btn2.setText("Hey");
+                //pane.setRight(btn2);
             }
         });
-       
-        Scene scene = new Scene(pane, 600, 500);
+       editBtn.setOnAction(new EventHandler<ActionEvent>(){
+          @Override
+          public void handle(ActionEvent event){
+              S.Edit();
+          }
+       });
+       deleteBtn.setOnAction(new EventHandler<ActionEvent>(){
+           @Override
+           public void handle(ActionEvent event){
+               S.Delete();
+           }
+       });
+       exitBtn.setOnAction(new EventHandler<ActionEvent>(){
+           @Override
+           public void handle(ActionEvent event){
+               System.exit(0);
+           }
+       });
+      //  Scene scene = new Scene(pane, 600, 500);
         
         primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+        //primaryStage.setScene(scene);
         primaryStage.show();
     }
 
---
+++ FinalProject/src/finalproject/Second.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Chris
+ */
+public class Second extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+      
+        primaryStage.show();
+    }
+public void Add()
+{
+    
+}
+public void Edit()
+{
+    
+}
+public void Delete()
+{
+    
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
